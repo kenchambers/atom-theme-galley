5055f6df18626af697114f1cbba9f0080e3ad1f1
kenchambers <ken@platejoy.com>
2021-05-15 10:35:09 -0700

    second commit

--- src/search-component.js
+++ src/search-component.js
@@ -63,32 +63,18 @@ function SearchComponent() {
   }, [])
 
   return (
-    <Grid>
-      <Grid.Column width={6}>
-        <Search
-          loading={loading}
-          onResultSelect={(e, data) =>
-            dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title })
-          }
-          onSearchChange={handleSearchChange}
-          results={results}
-          value={value}
-        />
-      </Grid.Column>
+    <>
+    <Search
+      loading={loading}
+      onResultSelect={(e, data) =>
+        dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title })
+      }
+      onSearchChange={handleSearchChange}
+      results={results}
+      value={value}
+    />
+    </>
 
-      <Grid.Column width={10}>
-        <Segment>
-          <Header>State</Header>
-          <pre style={{ overflowX: 'auto' }}>
-            {JSON.stringify({ loading, results, value }, null, 2)}
-          </pre>
-          <Header>Options</Header>
-          <pre style={{ overflowX: 'auto' }}>
-            {JSON.stringify(source, null, 2)}
-          </pre>
-        </Segment>
-      </Grid.Column>
-    </Grid>
   )
 }
 
